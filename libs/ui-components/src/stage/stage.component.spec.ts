f3312245c8b4e492ece4c2b8cb26858aa4454da7
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2025-02-20 10:42:01 +0000

    feat(IAIMC-4): implemented test for displaying default description,title and image

--- libs/ui-components/src/stage/stage.component.spec.ts
+++ libs/ui-components/src/stage/stage.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { StageComponent } from './stage.component';
 
 describe('StageComponent', () => {
@@ -18,4 +19,21 @@ describe('StageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display default title and description', () => {
+    const titleElement = fixture.debugElement.query(By.css('.stage-title'));
+    const descriptionElement = fixture.debugElement.query(By.css('.stage-description'));
+
+    expect(titleElement.nativeElement.textContent).toBe(component.title);
+    expect(descriptionElement.nativeElement.textContent).toBe(component.description);
+  });
+
+  it('should render image with default attributes', () => {
+    const imageElement = fixture.debugElement.query(By.css('.stage-image'));
+
+    expect(imageElement.nativeElement.src).toContain('/imd-stage.png');
+    expect(imageElement.nativeElement.alt).toBe('Injection molding mechanical part');
+  });
+
+
 });
