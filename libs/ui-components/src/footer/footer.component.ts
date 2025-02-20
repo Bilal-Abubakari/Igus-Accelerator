8300484927778799da5354249beab4b8ccba7a82
= <=>
2025-02-20 20:56:26 +0000

    feat(IAIMC-6): design the ui for the footer

--- libs/ui-components/src/footer/footer.component.ts
+++ libs/ui-components/src/footer/footer.component.ts
@@ -1,11 +1,41 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent {}
+export class FooterComponent {
+  hoveredRating = 0;
+  selectedRating = 0;
+
+  onMouseEnter(rating: number): void {
+    this.hoveredRating = rating;
+  }
+
+  onMouseLeave(): void {
+    this.hoveredRating = 0;
+  }
+
+  onClick(rating: number): void {
+    this.selectedRating = rating;
+  }
+}
