0ac437fc064b1527f4ae1f4c0ac0e4709d47e1e3
nalon03-github <nalongrace03@gmail.com>
2025-02-21 02:34:08 +0000

    feat(IAIMC-19): Merge changes from IAIMC-4 into current branch

--- apps/iaimc-frontend/src/app/app.routes.ts
+++ apps/iaimc-frontend/src/app/app.routes.ts
@@ -1,11 +1,3 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [
-  {
-    path: 'contact',
-    loadComponent: () =>
-      import('../app/contact-form/contact-form.component').then(
-        (c) => c.ContactFormComponent
-      ),
-  },
-];
+export const appRoutes: Route[] = [];
--- libs/ui-components/src/index.ts
+++ libs/ui-components/src/index.ts
@@ -1,1 +1,2 @@
 export * from './stage/stage.component';
+export * from './contact-form/contact-form.component'
