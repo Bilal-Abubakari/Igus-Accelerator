dc5b05745b33bfc9454122ce9b6aedc5cc8a440a
nalon03-github <nalongrace03@gmail.com>
2025-02-21 05:06:26 +0000

    fix(IAIMC-19): Fix app.component test failed checks

--- apps/iaimc-frontend/src/app/app.component.spec.ts
+++ apps/iaimc-frontend/src/app/app.component.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -10,7 +9,13 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should render title', () => {
+  it('should create the component', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should render title inside an h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
