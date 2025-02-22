8d0767e5442bc073946d25be8e0890ebee0a628d
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2025-02-22 18:03:37 +0000

    feat(IAIMC-4): fix code quality checks failing

--- libs/ui-components/src/stage/stage.component.spec.ts
+++ libs/ui-components/src/stage/stage.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { StageComponent } from './stage.component';
-import { ChangeDetectionStrategy } from '@angular/core';
 
 describe('StageComponent', () => {
   let component: StageComponent;
