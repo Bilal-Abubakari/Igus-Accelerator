07e0cbec822aeba73420a5e2aea88b94a05cabff
jaakumasi <jerome.akumasi@amalitech.com>
2025-02-25 06:20:23 +0000

    chore: configure typeorm module

--- apps/iaimc-backend/src/app/app.module.ts
+++ apps/iaimc-backend/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getPostgresConfig } from './configurations/database.config';
+import envConfig from './configurations/env.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import envConfig from '../configurations/env.config';
 
 @Module({
-  imports: [ConfigModule.forRoot(envConfig)],
+  imports: [
+    ConfigModule.forRoot(envConfig),
+    TypeOrmModule.forRootAsync({
+      useFactory: getPostgresConfig,
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
