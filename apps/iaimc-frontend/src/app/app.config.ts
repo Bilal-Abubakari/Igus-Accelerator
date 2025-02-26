250d46d06a211e6a0dd6f4a2e8e777707365ef87
josephKwarteng2 <joseph.kwarteng@amalitech.com>
2025-02-26 03:29:58 +0000

    feat(frontend): configure NgRx store and effects in app configuration

--- apps/iaimc-frontend/src/app/app.config.ts
+++ apps/iaimc-frontend/src/app/app.config.ts
@@ -1,11 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 import { appRoutes } from './app.routes';
+import { ModelsEffects } from './customer/store/model-upload/model-upload.effects';
+import { modelsReducer } from './customer/store/model-upload/model-upload.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideStore({ modelUpload: modelsReducer }),
+    provideEffects(ModelsEffects),
   ],
 };
