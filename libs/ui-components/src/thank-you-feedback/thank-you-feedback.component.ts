88c331ee6bd401f8055ec1b7a429022a20d9dd81
= <=>
2025-02-26 17:32:13 +0000

    feat(frontend): design the thank you feedback ui

---
+++ libs/ui-components/src/thank-you-feedback/thank-you-feedback.component.ts
@@ -0,0 +1,37 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-thank-you-feedback',
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    RouterLink,
+  ],
+  templateUrl: './thank-you-feedback.component.html',
+  styleUrl: './thank-you-feedback.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ThankYouFeedbackComponent {
+  contactForm!: FormGroup;
+  @Input() submitted = true;
+
+  constructor(private readonly fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      email: ['', [Validators.email, Validators.required]],
+    });
+  }
+}
