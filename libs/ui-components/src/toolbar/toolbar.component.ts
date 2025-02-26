27f32e9e0588b6dd0c58336911c34d3775917d94
Richard Derrick McAdams <richard.mcadams@amalitech.com>
2025-02-26 07:37:57 +0000

    feat(frontend): implemented toolbar component

---
+++ libs/ui-components/src/toolbar/toolbar.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-toolbar',
+  imports: [CommonModule, MatToolbar, MatIconModule, MatButtonModule],
+  templateUrl: './toolbar.component.html',
+  styleUrl: './toolbar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ToolbarComponent {}
