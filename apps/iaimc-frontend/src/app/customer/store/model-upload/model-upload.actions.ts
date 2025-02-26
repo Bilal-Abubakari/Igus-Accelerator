2fa0b1cfd1595c3801963167c12f47dca24ec188
josephKwarteng2 <joseph.kwarteng@amalitech.com>
2025-02-26 03:31:16 +0000

    feat(frontend): add NgRx actions for model upload feature

---
+++ apps/iaimc-frontend/src/app/customer/store/model-upload/model-upload.actions.ts
@@ -0,0 +1,15 @@
+import { createActionGroup, props, emptyProps } from '@ngrx/store';
+import { Model } from './model-upload.state';
+
+export const ModelsActions = createActionGroup({
+  source: 'Models',
+  events: {
+    'Upload Model': props<{ file: File }>(),
+    'Upload Model Success': props<{ model: Model }>(),
+    'Upload Model Failure': props<{ error: string }>(),
+
+    'Load Models': emptyProps(),
+    'Load Models Success': props<{ models: Model[] }>(),
+    'Load Models Failure': props<{ error: string }>(),
+  },
+});
