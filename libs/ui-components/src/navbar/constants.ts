c2d07d2a761366ccad5ae00973737a99b61d6d64
josephKwarteng2 <joseph.kwarteng@amalitech.com>
2025-02-26 02:11:30 +0000

    chore: add navigation routes constants for NavbarComponent

---
+++ libs/ui-components/src/navbar/constants.ts
@@ -0,0 +1,8 @@
+export const NAVIGATION_ROUTES = {
+  HOME: 'customer/library',
+  CONFIGURATIONS: 'customer/molding-configuration/configurations',
+  PRODUCIBILITY: 'customer/molding-configuration/producibility',
+  LIBRARY: 'library',
+  MOLDING_CONFIGURATION: 'molding-configuration',
+  CUSTOMER: 'customer',
+} as const;
