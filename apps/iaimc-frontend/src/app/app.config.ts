b613fcd782b88e6c273c046c7d89a230ae6c21e6
josephKwarteng2 <joseph.kwarteng@amalitech.com>
2025-03-01 15:13:22 +0000

    chore: configure Angular application with essential providers

--- apps/iaimc-frontend/src/app/app.config.ts
+++ apps/iaimc-frontend/src/app/app.config.ts
@@ -1,10 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(),
+    provideAnimations(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
   ],
