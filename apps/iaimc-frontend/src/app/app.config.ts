13ed330047aabe1e2710d3494ad40630b5b464ef
nalon03-github <nalongrace03@gmail.com>
2025-03-02 18:23:12 +0000

    fix(frontend): remove duplicate provideAnimationsAsync method

--- apps/iaimc-frontend/src/app/app.config.ts
+++ apps/iaimc-frontend/src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
-
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 
@@ -12,7 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
-    provideAnimationsAsync(),
     {
       provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
       useValue: { appearance: 'outline', hideRequiredMarker: true },
