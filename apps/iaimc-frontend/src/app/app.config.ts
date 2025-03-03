f2d74f1adbd86bb1abf0f663a45700bfc78d9dea
josephKwarteng2 <joseph.kwarteng@amalitech.com>
2025-03-03 15:59:23 +0000

    chore: point environment to local

--- apps/iaimc-frontend/src/app/app.config.ts
+++ apps/iaimc-frontend/src/app/app.config.ts
@@ -11,7 +11,7 @@ import { provideTranslocoPersistLang } from '@jsverse/transloco-persist-lang';
 import { appRoutes } from './app.routes';
 import { PrebuiltTranslocoLoader } from './transloco-loader';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { environment } from '../../environments/envronment.prod';
+import { environment } from '../../environments/environment';
 import {
   AVAILABLE_LANGUAGE_CODES,
   LANGUAGE_LOCALE_MAPPING,
