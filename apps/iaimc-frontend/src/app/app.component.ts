d73b72a532d10db8addc273065500ee78f27d9b4
josephKwarteng2 <joseph.kwarteng@amalitech.com>
2025-03-04 11:07:47 +0000

    fix(frontend): move navbar to AppComponent for consistent visibility

--- apps/iaimc-frontend/src/app/app.component.ts
+++ apps/iaimc-frontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '@igus-accelerator-injection-molding-configurator/ui-components';
+import { ToolbarComponent } from '../../../../libs/ui-components/src/toolbar/toolbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
