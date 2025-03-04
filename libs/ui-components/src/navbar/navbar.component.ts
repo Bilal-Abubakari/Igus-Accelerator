82e9e2d70a9b361a0c358bb680be14cc3bc70a69
josephKwarteng2 <joseph.kwarteng@amalitech.com>
2025-03-04 12:08:23 +0000

    feat(frontend): integrate upload state service into navbar component

--- libs/ui-components/src/navbar/navbar.component.ts
+++ libs/ui-components/src/navbar/navbar.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  signal,
+  inject,
+  computed,
+} from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -6,6 +12,7 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatButtonModule } from '@angular/material/button';
 import { NAVIGATION_ROUTES } from './constants';
 import { TranslocoPipe } from '@jsverse/transloco';
+import { UploadStateService } from 'apps/iaimc-frontend/src/app/service/upload-state.service';
 
 @Component({
   selector: 'app-navbar',
@@ -24,14 +31,14 @@ import { TranslocoPipe } from '@jsverse/transloco';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavbarComponent {
-  public homeRoute = signal(NAVIGATION_ROUTES.LIBRARY);
+  private uploadState = inject(UploadStateService);
 
+  public homeRoute = signal(NAVIGATION_ROUTES.LIBRARY);
   public configurationsRoute = signal([
     '/',
     NAVIGATION_ROUTES.MOLDING_CONFIGURATION,
     NAVIGATION_ROUTES.CONFIGURATIONS,
   ]);
-
   public producibilityRoute = signal([
     '/',
     NAVIGATION_ROUTES.MOLDING_CONFIGURATION,
@@ -39,6 +46,8 @@ export class NavbarComponent {
   ]);
 
   public isMenuOpened = signal(false);
+
+  public hasUploadedFile = computed(() => this.uploadState.hasUploadedFile());
 
   public toggleMenu(): void {
     this.isMenuOpened.set(!this.isMenuOpened());
