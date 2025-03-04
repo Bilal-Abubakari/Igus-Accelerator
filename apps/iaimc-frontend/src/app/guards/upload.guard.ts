5cf961a585de8464e3a1cfdf30b2c2aaa43aceb6
josephKwarteng2 <joseph.kwarteng@amalitech.com>
2025-03-04 12:07:03 +0000

    feat(frontend): Add UploadGuard to protect routes based on upload state

---
+++ apps/iaimc-frontend/src/app/guards/upload.guard.ts
@@ -0,0 +1,19 @@
+import { inject, Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { UploadStateService } from '../service/upload-state.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UploadGuard implements CanActivate {
+  private readonly uploadState = inject(UploadStateService);
+  private router = inject(Router);
+
+  public canActivate(): boolean {
+    if (!this.uploadState.hasUploadedFile()) {
+      this.router.navigate(['/']);
+      return false;
+    }
+    return true;
+  }
+}
