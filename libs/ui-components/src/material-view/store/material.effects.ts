b2e1c815e9b3132098db617ba07a75a6d11dcecd
josephKwarteng2 <joseph.kwarteng@amalitech.com>
2025-03-08 01:26:02 +0000

    feat(frontend): add MaterialEffects to handle material loading

---
+++ libs/ui-components/src/material-view/store/material.effects.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { MaterialService } from '../service/material.service';
+import { MaterialActions } from './material.actions';
+import { catchError, map, mergeMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { Material } from './material.model';
+import { HttpErrorResponse } from '@angular/common/http';
+
+@Injectable()
+export class MaterialEffects {
+  constructor(
+    private readonly actions$: Actions,
+    private readonly materialService: MaterialService,
+  ) {
+    this.loadMaterials$ = createEffect(() =>
+      this.actions$.pipe(
+        ofType(MaterialActions.loadMaterials),
+        mergeMap(() =>
+          this.materialService.getMaterials().pipe(
+            map((materials: Material[]) =>
+              MaterialActions.loadMaterialsSuccess({ materials }),
+            ),
+            catchError((error: HttpErrorResponse) =>
+              of(
+                MaterialActions.loadMaterialsFailure({
+                  materialFetchError: error.message,
+                }),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  loadMaterials$;
+}
