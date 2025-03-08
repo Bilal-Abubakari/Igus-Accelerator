13525bab113a74795422194c723ba114e17f03f5
jaakumasi <jerome.akumasi@amalitech.com>
2025-03-08 19:34:54 +0000

    chore: cors should allow requests from the right preview frontend urls

--- apps/iaimc-backend/src/main.ts
+++ apps/iaimc-backend/src/main.ts
@@ -1,6 +1,8 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { ValidationPipe, INestApplication } from '@nestjs/common';
+
+type CorsOriginsFn = (error: Error | null, allow: boolean) => void;
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +21,9 @@ async function bootstrap(): Promise<void> {
 }
 
 const setupCors: (app: INestApplication) => void = (app: INestApplication) => {
+  const previewUrlPattern =
+    /^https:\/\/preview-injection-moulding-configurator-[a-z0-9]+\.vercel\.app$/;
+
   const allowedOrigins = [
     'https://injection-moulding-configurator.vercel.app',
     process.env.NODE_ENV === 'development'
@@ -26,8 +31,19 @@ const setupCors: (app: INestApplication) => void = (app: INestApplication) => {
       : '',
   ];
 
+  const corsOriginsFn: (origin: string, cb: CorsOriginsFn) => void = (
+    origin: string,
+    cb: CorsOriginsFn,
+  ) => {
+    if (allowedOrigins.includes(origin) || previewUrlPattern.test(origin)) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+    }
+  };
+
   app.enableCors({
-    origin: allowedOrigins,
+    origin: corsOriginsFn,
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
