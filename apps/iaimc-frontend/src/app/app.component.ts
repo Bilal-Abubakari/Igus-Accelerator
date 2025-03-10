43c403590e53a76e11591772b6c627b88c54426e
mcthekreator <mcadamsrd@gmail.com>
2025-03-10 16:44:09 +0000

    refactor(frontend): rearange landing page and model library components

--- apps/iaimc-frontend/src/app/app.component.ts
+++ apps/iaimc-frontend/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from '../../../../libs/ui-components/src/landing-page/toolbar/toolbar.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
