895d69d5c7eaff04024bce8f7eb0e835d0814904
josephKwarteng2 <joseph.kwarteng@amalitech.com>
2025-03-10 19:37:30 +0000

    refactor(frontend): remove direct constructor assignment

--- libs/ui-components/src/materials/store/material.effects.ts
+++ libs/ui-components/src/materials/store/material.effects.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { MaterialService } from '../service/material.service';
 import { MaterialActions } from './material.actions';
@@ -9,30 +9,26 @@ import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable()
 export class MaterialEffects {
-  constructor(
-    private readonly actions$: Actions,
-    private readonly materialService: MaterialService,
-  ) {
-    this.loadMaterials$ = createEffect(() =>
-      this.actions$.pipe(
-        ofType(MaterialActions.loadMaterials),
-        mergeMap(() =>
-          this.materialService.getMaterials().pipe(
-            map((materials: Material[]) =>
-              MaterialActions.loadMaterialsSuccess({ materials }),
-            ),
-            catchError(({ message }: HttpErrorResponse) =>
-              of(
-                MaterialActions.loadMaterialsFailure({
-                  materialFetchError: message,
-                }),
-              ),
+  private readonly actions$ = inject(Actions);
+  private readonly materialService = inject(MaterialService);
+
+  loadMaterials$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(MaterialActions.loadMaterials),
+      mergeMap(() =>
+        this.materialService.getMaterials().pipe(
+          map((materials: Material[]) =>
+            MaterialActions.loadMaterialsSuccess({ materials }),
+          ),
+          catchError(({ message }: HttpErrorResponse) =>
+            of(
+              MaterialActions.loadMaterialsFailure({
+                materialFetchError: message,
+              }),
             ),
           ),
         ),
       ),
     );
-  }
-
-  loadMaterials$;
+  });
 }
