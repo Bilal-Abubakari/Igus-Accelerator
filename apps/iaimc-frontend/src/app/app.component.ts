18fda99626cd62d1e5195e2e2a08e4f6d4667cdb
mcthekreator <mcadamsrd@gmail.com>
2025-03-10 15:19:39 +0000

    refactor(frontend): create a landing page component and added it to route

--- apps/iaimc-frontend/src/app/app.component.ts
+++ apps/iaimc-frontend/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from '@igus-accelerator-injection-molding-configurator/ui-components';
-import { ToolbarComponent } from '../../../../libs/ui-components/src/landing-page/toolbar/toolbar.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, ToolbarComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
--- libs/ui-components/src/navbar/constants.ts
+++ libs/ui-components/src/navbar/constants.ts
@@ -1,8 +1,10 @@
 export const NAVIGATION_ROUTES = {
+
   MOLDING_CONFIGRATION_CONFIGURATIONS: 'configurations',
   MOLDING_CONFIGRATION_PRODUCIBILITY: 'producibility',
   LIBRARY: 'library',
   MOLDING_CONFIGURATION: 'molding-configuration',
   CONFIGURATIONS: 'configurations',
   PRODUCIBILITY: 'producibility',
+  LANDING_PAGE: 'home'
 } as const;
