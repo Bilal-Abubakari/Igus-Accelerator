386e66ed2ce3a66f86ccb1fd0011fce226e8eb39
josephKwarteng2 <joseph.kwarteng@amalitech.com>
2025-03-11 13:40:54 +0000

    chore: restructure imports

--- apps/iaimc-frontend/src/app/app.reducer.ts
+++ apps/iaimc-frontend/src/app/app.reducer.ts
@@ -8,18 +8,18 @@ import {
   footerReducer,
 } from '../../../../libs/ui-components/src/model/components/main-footer/store/footer.reducer';
 import { MaterialState } from 'libs/ui-components/src/materials/store/material.state';
-
-export interface AppState {
-  [MATERIAL_FEATURE_KEY]: MaterialState;
-  [FOOTER_FEATURE_KEY]: FooterState;
-  [CONTACT_FORM_FEATURE_KEY]: ContactFormState;
-}
 import {
   CONTACT_FORM_FEATURE_KEY,
   contactFormReducer,
 } from 'libs/ui-components/src/contact-form/store/contact-form.reducer';
 import { ContactFormState } from 'libs/ui-components/src/contact-form/store/contact-form.models';
 import { FooterState } from 'libs/ui-components/src/model/components/main-footer/store/footer.state';
+
+export interface AppState {
+  [MATERIAL_FEATURE_KEY]: MaterialState;
+  [FOOTER_FEATURE_KEY]: FooterState;
+  [CONTACT_FORM_FEATURE_KEY]: ContactFormState;
+}
 
 export const appReducer: ActionReducerMap<AppState> = {
   [MATERIAL_FEATURE_KEY]: materialReducer,
