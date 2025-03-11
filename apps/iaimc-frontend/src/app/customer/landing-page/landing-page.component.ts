8ceff711d913b444fc6f038b09aa69a328cf246b
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2025-03-11 11:39:03 +0000

    feat(frontend): add schedule a call section to landing page component

--- apps/iaimc-frontend/src/app/customer/landing-page/landing-page.component.ts
+++ apps/iaimc-frontend/src/app/customer/landing-page/landing-page.component.ts
@@ -2,12 +2,18 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {
   StageComponent,
   MaterialSelectionComponent,
+  ScheduleCallComponent,
 } from '@igus-accelerator-injection-molding-configurator/ui-components';
 import { ModelUploadComponent } from '../../../../../../libs/ui-components/src/model/components/model-upload/model-upload.component';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [StageComponent, ModelUploadComponent, MaterialSelectionComponent],
+  imports: [
+    StageComponent,
+    ModelUploadComponent,
+    MaterialSelectionComponent,
+    ScheduleCallComponent,
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
