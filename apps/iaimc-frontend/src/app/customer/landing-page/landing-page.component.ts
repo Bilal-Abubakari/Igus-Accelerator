783380a637e2303612b9a38cb6c626b4aa7ef0e9
mcthekreator <mcadamsrd@gmail.com>
2025-03-11 13:58:44 +0000

    feat(frontend): added how it works library to ladning page component

--- apps/iaimc-frontend/src/app/customer/landing-page/landing-page.component.ts
+++ apps/iaimc-frontend/src/app/customer/landing-page/landing-page.component.ts
@@ -2,12 +2,18 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {
   StageComponent,
   MaterialSelectionComponent,
+  HowItWorksComponent,
 } from '@igus-accelerator-injection-molding-configurator/ui-components';
 import { ModelUploadComponent } from '../../../../../../libs/ui-components/src/model/components/model-upload/model-upload.component';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [StageComponent, ModelUploadComponent, MaterialSelectionComponent],
+  imports: [
+    StageComponent,
+    ModelUploadComponent,
+    MaterialSelectionComponent,
+    HowItWorksComponent,
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
