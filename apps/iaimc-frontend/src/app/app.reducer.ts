54cfeaf5d3eea77dc68917553e05dfb5b44cfa26
josephKwarteng2 <joseph.kwarteng@amalitech.com>
2025-03-11 10:32:25 +0000

    chore: add contact form state to app state

--- apps/iaimc-frontend/src/app/app.reducer.ts
+++ apps/iaimc-frontend/src/app/app.reducer.ts
@@ -13,11 +13,13 @@ import { MaterialState } from 'libs/ui-components/src/materials/store/material.s
 export interface AppState {
   [MATERIAL_FEATURE_KEY]: MaterialState;
   [FOOTER_FEATURE_KEY]: FooterState;
+  [CONTACT_FORM_FEATURE_KEY]: ContactFormState;
 }
 import {
   CONTACT_FORM_FEATURE_KEY,
   contactFormReducer,
 } from 'libs/ui-components/src/contact-form/store/contact-form.reducer';
+import { ContactFormState } from 'libs/ui-components/src/contact-form/store/contact-form.models';
 
 export const appReducer: ActionReducerMap<AppState> = {
   [MATERIAL_FEATURE_KEY]: materialReducer,
