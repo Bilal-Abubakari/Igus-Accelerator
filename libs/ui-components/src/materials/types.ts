47d7eeeb3792af2906b340485657febc72090964
josephKwarteng2 <joseph.kwarteng@amalitech.com>
2025-03-13 22:35:37 +0000

    feat(frontend): add ViewMode type,  MATERIAL_COLUMNS constant and util

---
+++ libs/ui-components/src/materials/types.ts
@@ -0,0 +1,8 @@
+export type ViewMode = 'grid' | 'table';
+export const MATERIAL_COLUMNS = [
+  'name',
+  'moreInfo',
+  'deliveryTime',
+  'tolerance',
+  'price',
+];
---
+++ libs/ui-components/src/utilities/materials.utils.ts
@@ -0,0 +1,3 @@
+export function tolerancePercentage(shrinkage: number): number {
+  return shrinkage * 100;
+}
