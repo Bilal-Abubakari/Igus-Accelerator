b2e4d1dfb698c004a75b344b043f7664f5a4c905
josephKwarteng2 <joseph.kwarteng@amalitech.com>
2025-03-14 07:14:28 +0000

    feat(frontend): conditionally render table component and card component in materials page

--- libs/ui-components/src/materials/materials.component.ts
+++ libs/ui-components/src/materials/materials.component.ts
@@ -17,10 +17,27 @@ import { MaterialActions } from './store/material.actions';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
 import { MaterialsCardComponent } from './components/materials-card/materials-card.component';
 import { TranslocoPipe } from '@jsverse/transloco';
+import { ViewMode } from './types';
+import { MaterialsTableComponent } from './components/materials-table/materials-table.component';
+import {
+  MatButtonToggle,
+  MatButtonToggleGroup,
+} from '@angular/material/button-toggle';
+import { FormsModule } from '@angular/forms';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-materials',
-  imports: [MatProgressSpinner, MaterialsCardComponent, TranslocoPipe],
+  imports: [
+    MatProgressSpinner,
+    MaterialsCardComponent,
+    TranslocoPipe,
+    MaterialsTableComponent,
+    MatButtonToggleGroup,
+    MatButtonToggle,
+    FormsModule,
+    MatIcon,
+  ],
   templateUrl: './materials.component.html',
   styleUrl: './materials.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -32,8 +49,8 @@ export class MaterialsComponent implements OnInit {
   public readonly loading = this.store.selectSignal(selectLoading);
   public readonly error = this.store.selectSignal(selectError);
   public readonly selectedMaterialId = this.store.selectSignal(selectMaterial);
-
   public readonly hasError: Signal<boolean> = computed(() => !!this.error());
+  public readonly viewMode: ViewMode = 'grid';
 
   ngOnInit(): void {
     this.store.dispatch(MaterialActions.loadMaterials());
