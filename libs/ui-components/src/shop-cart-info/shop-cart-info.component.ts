1c15a53ef8e97fa8782f5c8f89163a12840ca16d
Ahadjie-Bless <bless.ahadjie@amalitech.com>
2025-03-14 09:58:26 +0000

    feat(frontend): handle validation error gracefully

--- libs/ui-components/src/shop-cart-info/shop-cart-info.component.ts
+++ libs/ui-components/src/shop-cart-info/shop-cart-info.component.ts
@@ -1,10 +1,9 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, FormsModule, Validators } from '@angular/forms';
 import { TranslocoPipe } from '@jsverse/transloco';
 import { ReusableButtonComponent } from '../reusable-components/reusable-button/reusable-button.component';
 import { ReusableFormFieldComponent } from '../reusable-components/reusable-form-field/reusable-form-field.component';
-import { DEFAULT_ERROR_MESSAGES } from '../utilities/error-messages';
 
 @Component({
   selector: 'app-shop-cart-info',
@@ -19,18 +18,15 @@ import { DEFAULT_ERROR_MESSAGES } from '../utilities/error-messages';
   styleUrl: './shop-cart-info.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ShopCartInfoComponent implements OnInit {
-  public readonly customErrorMessages = DEFAULT_ERROR_MESSAGES;
+export class ShopCartInfoComponent {
   public readonly price = -1;
-  public quantity: number | null = 1;
+
   public quantityControl = new FormControl(1, [
     Validators.required,
     Validators.min(1),
   ]);
 
-  ngOnInit() {
-    this.quantityControl.valueChanges.subscribe((value) => {
-      this.quantity = value;
-    });
-  }
+  public readonly errorMessages = {
+    min: 'Value must not be below 1',
+  };
 }
